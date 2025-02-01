8a04ca09ce63a27783067510d253b952852319c2
CoderTanu <tk6353129@gmail.com>
2025-02-01 21:39:00 +0530

    feb potd leetcode

---
+++ LeetcodePOTD/src/February_POTD/Day1_Easy_String.java
@@ -0,0 +1,28 @@
+package February_POTD;
+
+
+/**
+ * 3151. Special Array I
+ * 
+ * An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+ * 
+ */
+public class Day1_Easy_String {
+
+	public static boolean specialArray(int s[]) {
+		int n = s.length;
+		if(n == 1) return true;
+		for(int i=1; i<n; i++) {
+			if((s[i] % 2) ==(s[i-1]%2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		int s[] = {2,1,4};
+		System.out.println(specialArray(s));
+	}
+
+}
